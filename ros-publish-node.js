6f1f8aa2d71744bea934be73fd2daf209c9d2170
Jorrit Goos <jorrit.goos@tno.nl>
2020-11-13 15:14:22 +0100

    Added checkbox to inject ROS time stamp in header before publishing msg

--- ros-publish-node.js
+++ ros-publish-node.js
@@ -1,6 +1,7 @@
 module.exports = function(RED) {
   return function (config) {
     var ROSLIB = require('roslib'); 
+    const rosnodejs = require('rosnodejs')
 
     RED.nodes.createNode(this,config);
     var node = this;
@@ -21,8 +22,26 @@ module.exports = function(RED) {
       topic.ros = node.server.ros;
       // node.log('publishing msg ' + msg.payload);
       // var pubslishMsg = new ROSLIB.Message({data: msg.payload});
-      topic.publish(msg.payload);
+      var new_payload = msg.payload;
+      // Insert timestamp in header
+      if (config.stampheader){
+        const Time = rosnodejs.Time;
+        const now = Time.now();
+        new_payload = addHeader(new_payload, now);
+      }
+      topic.publish(new_payload);
     });
+
+    function addHeader(payload_, now_)
+    {      
+      if ('header' in payload_){
+        payload_.header.stamp = now_;
+      }
+      else{
+        node.error('Cannot add stamp to header, because incoming msg does not contain a header!');
+      }
+      return payload_;
+    }
 
     node.server.on('ros connected', () => {
       node.status({fill:"green",shape:"dot",text:"connected"});
